95a7387ffb5aefc04efaf8918646d6f5161dfba5
Kim Kwangtae <91174156+z3zzz@users.noreply.github.com>
2022-06-15 14:59:06 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { store } from './state';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <div>Hello!</div>
-  </React.StrictMode>
+  </Provider>
 );
