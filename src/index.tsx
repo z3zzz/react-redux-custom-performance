945477708b4b1096572de508bf9ccfef780dbcd8
z3zzz <zabra7946@naver.com>
2022-06-25 21:42:48 +0900

    Apply react-router in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './state';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <Provider store={store}>
     <div>Hello!</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>home</div>} />
+        <Route path="/login" element={<div>login</div>} />
+        <Route path="/register" element={<div>register</div>} />
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
